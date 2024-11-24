9cd91cfab84adac65b6e2b72d6d6bb0f38711b44
alotanna <austineihejiben12@gmail.com>
2024-11-24 06:04:07 +0000

    css for services

--- view/services.php
+++ view/services.php
@@ -49,6 +49,10 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelenne Car Wash - Services Management</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../assets/css/inventory.css">
+    <link rel="stylesheet" href="../assets/css/dashboardservices.css">
     <link rel="stylesheet" href="../assets/css/dashboard.css">
     <style>
         :root {
